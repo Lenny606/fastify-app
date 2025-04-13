afa8b8052ca9510529aab07c2fce5130549a43c8
Tomas Kravcik <kravcik@procorp.cz>
2025-04-13 21:24:16 +0200

    Implement environment variable management using @fastify/env and update server startup logic in app.js

--- app.js
+++ app.js
@@ -1,19 +1,54 @@
+import fastifyEnv from '@fastify/env'
 import Fastify from 'fastify'
 
 const fastify = Fastify({
-  logger: true
+    logger: true
 })
+
+// SCHEMA
+const schema = {
+    type: 'object',
+    required: ['PORT'],
+    properties: {
+        PORT: {
+            type: "string",
+            default: 3000
+        }
+    }
+}
+
+const options = {
+    confKey: "config",
+    dotenv: true,
+    schema: schema,
+    // data : data
+}
+
+fastify.register(fastifyEnv, options).ready(
+    (err) => {
+        if (err) {
+            console.error('Error loading environment variables:', err)
+            process.exit(1)
+        }
+    }
+)
 
 // Declare a route
 fastify.get('/', function (request, reply) {
-  reply.send({ hello: 'world' })
+    reply.send({ hello: 'world' })
 })
 
-// Run the server!
-fastify.listen({ port: 3000 }, function (err, address) {
-  if (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
-  // Server is now listening on ${address}
-})
+console.log( fastify.config);
+
+
+const start = async () => {
+    try {
+        fastify.listen({ port: 3000 })
+        fastify.log.info(`Server is running `)
+    } catch (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+}
+
+start()
